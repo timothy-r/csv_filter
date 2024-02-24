a6bcaf0aa291424976cca4328025533ee064b30b
timothy-r <tim.rodger@gmail.com>
2024-02-24 08:15:14 +0000

    Updates table filter builder tests

--- csv_filter/tests/table_filter_builder_test.py
+++ csv_filter/tests/table_filter_builder_test.py
@@ -3,6 +3,7 @@ import unittest
 from csv_filter.filter.table_filter_builder import TableFilterBuilder
 from csv_filter.filter.table_filter import TableFilter
 from csv_filter.filter.single_condition_filter import SingleConditionFilter
+from csv_filter.filter.two_condition_filter import TwoConditionFilter
 
 from csv_filter.query.operator import Operator
 from csv_filter.query.condition import Condition
@@ -29,4 +30,15 @@ class TableFilterBuilderTest(unittest.TestCase):
 
         table_filter = self._builder.build()
         self.assertIsInstance(table_filter, SingleConditionFilter)
+
+    def test_build_two_condition_filter(self) -> None:
+        condition_a = Condition('A', Comparision.EQUALS, '5')
+        condition_b = Condition('B', Comparision.GREATER_THAN, '100')
+
+        self._builder.add_condition(condition=condition_a)
+        self._builder.add_operator(Operator.AND)
+        self._builder.add_condition(condition=condition_b)
+
+        table_filter = self._builder.build()
+        self.assertIsInstance(table_filter, TwoConditionFilter)
 
